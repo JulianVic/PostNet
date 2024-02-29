33a7a4a2ac83641452a585eb4f159500fb0d91e6
JulianVic <josejulianvicenteperez@gmail.com>
2024-02-29 01:47:13 -0600

    Adding CORS

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express from "express";
+import cors from "cors";
 import { postRouter } from "./Post/infraestructure/routes/postRouter";
 
 const app = express();
 const PORT = process.env.PORT;
+const client = process.env.CLIENT_URL!;
 
 app.use(express.json());
+app.use(cors({
+  origin: [client],
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
+
+
 app.use("/api/post", postRouter);
 
 app.listen(PORT, () => {
