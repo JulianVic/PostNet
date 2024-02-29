dc58a694bd3c6b798897b8e40c46aba9fd3f8f7c
JulianVic <josejulianvicenteperez@gmail.com>
2024-02-28 19:13:53 -0600

    Deleting UserIdentity

--- src/User/domain/repositories/IEncryptRepository.ts
+++
@@ -1,4 +0,0 @@
-export interface IEncryptRepository {
-    encrypt(password: string): Promise<string>;
-    compare(password: string, hash: string): Promise<boolean>;
-}
--- src/User/domain/repositories/IUserRepository.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "../entities/User";
-
-export interface IUserRepository {
-    create(user: User): Promise<User>
-    readAll(): Promise<User[]> 
-    readById(id: number): Promise<User>
-}
